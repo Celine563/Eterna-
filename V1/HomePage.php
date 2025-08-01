74fae46224cfa7555791204899679ced37f134d3
Celine <B00160080@mytudublin.ie>
2025-08-01 12:09:16 +0100

    Initial commit

---
+++ V1/HomePage.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<link rel="stylesheet" type="text/css" href="CSS/HomeStyle.css">
+
+</head>
+
+<body>
+
+
+<div class="categories">
+    <h1><a href="HomePage.php">ETERNA</a></h1>
+    
+    <div class="dropdown">
+    <h3>Women</h3>
+    <ul class="dropdown-menu">
+        <li><a href="#">Dresses</a></li>
+        <li><a href="#">Shoes</a></li>
+        <li><a href="#">Accessories</a></li>
+    </ul>
+</div>
+
+
+    <div class="dropdown">
+        <h3>Men</h3>
+        <ul class="dropdown-menu">
+            <li><a href="#">Suits</a></li>
+            <li><a href="#">Casual</a></li>
+            <li><a href="#">Shoes</a></li>
+        </ul>
+    </div>
+
+    <div class="dropdown">
+        <h3>Children</h3>
+        <ul class="dropdown-menu">
+            <li><a href="#">Clothing</a></li>
+            <li><a href="#">Toys</a></li>
+        </ul>
+    </div>
+
+    <div class="dropdown">
+        <h3>Bags</h3>
+        <ul class="dropdown-menu">
+            <li><a href="#">Handbags</a></li>
+            <li><a href="#">Backpacks</a></li>
+        </ul>
+    </div>
+
+    <div class="dropdown">
+        <h3>Beauty</h3>
+        <ul class="dropdown-menu">
+            <li><a href="#">Skincare</a></li>
+            <li><a href="#">Makeup</a></li>
+        </ul>
+    </div>
+
+    <div class="dropdown">
+        <h3>Our Story</h3>
+        <ul class="dropdown-menu">
+            <li><a href="#">About Us</a></li>
+            <li><a href="#">Mission</a></li>
+        </ul>
+    </div>
+
+    <img src="Images/Search.png" alt="search" class="search">
+    <img src="Images/Account.png" alt="account" class="account">
+    <img src="Images/shoppingbag.png" alt="shopping bag" class="shoppingBag">
+</div>
+
+
+
+
+</body>
+</html>
---
+++ V2/header.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<link rel="stylesheet" type="text/css" href="CSS/header.css">
+
+</head>
+
+<body>
+
+
+<header class="top">
+    <h1><a href="HomePage.php">ETERNA</a></h1>
+    
+    <div class="dropdown">
+    <h3>Women</h3>
+    <ul class="menu">
+        <li><a href="dresses.php">Dresses</a></li>
+        <li><a href="shoes.php">Shoes</a></li>
+        <li><a href="accessories.php">Accessories</a></li>
+    </ul>
+</div>
+
+
+    <div class="dropdown">
+        <h3>Men</h3>
+        <ul class="menu">
+            <li><a href="#">Suits</a></li>
+            <li><a href="#">Casual</a></li>
+            <li><a href="#">Shoes</a></li>
+        </ul>
+    </div>
+
+    <div class="dropdown">
+        <h3>Children</h3>
+        <ul class="menu">
+            <li><a href="#">Clothing</a></li>
+            <li><a href="#">Toys</a></li>
+        </ul>
+    </div>
+
+    <div class="dropdown">
+        <h3>Bags</h3>
+        <ul class="menu">
+            <li><a href="#">Handbags</a></li>
+            <li><a href="#">Backpacks</a></li>
+        </ul>
+    </div>
+
+    <div class="dropdown">
+        <h3>Beauty</h3>
+        <ul class="menu">
+            <li><a href="#">Skincare</a></li>
+            <li><a href="#">Makeup</a></li>
+        </ul>
+    </div>
+
+    <div class="dropdown">
+        <h3>Our Story</h3>
+        <ul class="menu">
+            <li><a href="#">About Us</a></li>
+            <li><a href="#">Mission</a></li>
+        </ul>
+    </div>
+
+    <img src="Images/Search.png" alt="search" class="search">
+    <a href="account.php">
+  <img src="Images/Account.png" alt="account" class="account">
+</a>
+    <a href="cart.php">
+  <img src="Images/shoppingbag.png" alt="shopping bag" class="shoppingBag">
+</a>
+</header>
+
+
+
+
+</body>
+</html>
