8e71b337933781f812f471fa7ed40a101ca51a7c
Celine <B00160080@mytudublin.ie>
2025-08-06 12:25:06 +0100

    footer

--- V3/footer.php
+++ V3/footer.php
@@ -7,7 +7,7 @@
 <body>
 
 <footer class="bottom">
-
+<div class="newsletter">
 <h3>Sign up to our newsletter</h3>
 
 <form action="subscribe.php" method="POST">
@@ -15,13 +15,15 @@
   <button type="submit">Subscribe</button>
 </form>
 <p>By clicking on "Subscribe", you confirm that <br>you have read and understood our <a href="privacypolicy.php">Privacy Policy</a> <br> and that you want to receive the newsletter and <br> other marketing communication as explained.</p>
+</div>
 
-
-
-
-
-
-
+<div class="services">
+<h3>Services</h3>
+<a href="FAQ.php">FAQ</a>
+<br>
+<br>
+<a href="CareandService.php">Care & Service</a>
+</div>
 </footer>
 
 
