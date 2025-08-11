c2c96efc94e12e6e16eb3673707628138eb82248
Celine <B00160080@mytudublin.ie>
2025-08-11 13:49:52 +0100

    products page

---
+++ V3/productsPage.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <link rel="stylesheet" type="text/css" href="CSS/HomeStyle.css">
+  <link rel="stylesheet" type="text/css" href="CSS/product.css">
+</head>
+<body>
+
+<?php require 'header.php'; ?>
+
+<br>
+
+<img src="Images/brownheel.png" alt="Brown heel" />
+
+<br>
+<br>
+
+<?php require 'footer.php'; ?>
+<br>
+<?php require 'social.php'; ?>
+
+
+
+</body>
+</html>
